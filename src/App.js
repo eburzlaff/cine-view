13b40dd689582cde42d9c97dabfbedf704f438dd
eburzlaff <du.di.99@hotmail.com>
2023-07-15 00:42:28 -0500

    changing favicon and adding background image

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-
     setMovies(data.Search);
   };
 
